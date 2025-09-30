fec2e406c17efc372256805de1b7fe17e2dc46cd
setya.raka <setya.raka@elefin.id>
2025-09-30 11:49:53 +0700

    fix(api): ensure request body is valid for fetch when using plain objects

---
+++ src/services/auth.ts
@@ -0,0 +1,35 @@
+import { request } from "@/lib/api";
+import {
+  LoginBody,
+  LoginResp,
+  MeResp,
+  MyTenantsResp,
+  AssumeTenantByIdBody,
+  AssumeTenantResp,
+} from "@/types/api";
+
+export const AuthApi = {
+  login(body: LoginBody) {
+    return request<LoginResp>("/auth/login", {
+      method: "POST",
+      body,
+      tenantScoped: false, // belum punya tenant
+    });
+  },
+  me() {
+    return request<MeResp>("/auth/me", { method: "GET", tenantScoped: false });
+  },
+  myTenants() {
+    return request<MyTenantsResp>("/auth/me/tenants", {
+      method: "GET",
+      tenantScoped: false,
+    });
+  },
+  assumeTenantById(body: AssumeTenantByIdBody) {
+    return request<AssumeTenantResp>("/auth/assume-tenant-by-id", {
+      method: "POST",
+      body,
+      tenantScoped: false,
+    });
+  },
+};
